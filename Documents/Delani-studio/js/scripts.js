38123b830001adc7539a2f648d52ad083b5b128e
virsail <virsaileric@gmail.com>
2020-07-28 00:50:35 +0300

    add alert to notify user the message has been received

--- Documents/Delani-studio/js/scripts.js
+++ Documents/Delani-studio/js/scripts.js
@@ -31,11 +31,45 @@ $(document).ready(function() {
   $(".cruiser1").mouseleave(function(){
     $("#j2").hide()
   })
+  $(".cruiser2").mouseover(function() {
+    $("#j3").show()
+  })
+  $(".cruiser2").mouseleave(function(){
+    $("#j3").hide()
+  })
+  $(".cruiser3").mouseover(function() {
+    $("#j4").show()
+  })
+  $(".cruiser3").mouseleave(function(){
+    $("#j4").hide()
+  })
+  $(".cruiser4").mouseover(function() {
+    $("#j5").show()
+  })
+  $(".cruiser4").mouseleave(function(){
+    $("#j5").hide()
+  })
+  $(".cruiser5").mouseover(function() {
+    $("#j6").show()
+  })
+  $(".cruiser5").mouseleave(function(){
+    $("#j6").hide()
+  })
+  $(".cruiser6").mouseover(function() {
+    $("#j7").show()
+  })
+  $(".cruiser6").mouseleave(function(){
+    $("#j7").hide()
+  })
+  $(".cruiser7").mouseover(function() {
+    $("#j8").show()
+  })
+  $(".cruiser7").mouseleave(function(){
+    $("#j8").hide()
+  })
 })
-$(".cruiser2").mouseover(function() {
-  $("#j3").show()
-})
-$(".cruiser2").mouseleave(function(){
-  $("#j3").hide()
-})
-})
+$(document).ready(function(){
+  $(".section6").submit(function(){
+    alert("WE have received your message ,thank you for reaching out to Delani studio");
+  });
+});
