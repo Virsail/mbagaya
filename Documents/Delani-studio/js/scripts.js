e0ff93b606ce00d5c22140346f5c6acb37fa2f18
virsail <virsaileric@gmail.com>
2020-07-27 16:51:32 +0300

    create a toggle effect for design

--- Documents/Delani-studio/js/scripts.js
+++ Documents/Delani-studio/js/scripts.js
@@ -1,5 +1,13 @@
 alert("What's good awesome people,welcome to Delani Studio.It all happens here!");
-$(`.design`).click(function(){
-    $(`.design img`).toggle();
-    $(`.design p`).toggle();
-})
+$(document).ready(function() {
+  $(".toggle").click(function() {
+    $("#shown-design").toggle();
+    $("#hidden-design").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".toggle").click(function() {
+    $("#shown-development").toggle();
+    $("#hidden-development").toggle();
+  });
+});
